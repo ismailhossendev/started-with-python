37742e75da497b8d93c92b5eb043c46766ca64ed
ismallhossendev <ismailhossaindev@gmail.com>
2022-12-28 13:41:45 +0600

    variable in python

---
+++ variable.py
@@ -0,0 +1,36 @@
+# string variable 
+
+# name = "John"
+# print(name)
+# home = "New York"
+# print(home)
+# print(type(name))
+
+
+# string to integer 
+
+# age = "25"
+# print(type(age))
+# intAge = int(age)
+# print(type(intAge))
+
+# integer to string
+
+# age = 25
+# print(type(age))
+# strAge = str(age)
+# print(type(strAge))
+
+# string to float
+
+# age = "25"
+# print(type(age))
+# floatAge = float(age)
+# print(type(floatAge))
+# print(floatAge)
+
+# boolean
+
+# isTrue = True
+# isFalse = False
+# print(isTrue, isFalse)
