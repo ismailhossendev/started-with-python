8250e4fcd89df0bd1f5596dbbb1db00474845719
ismallhossendev <ismailhossaindev@gmail.com>
2022-12-28 14:25:13 +0600

    data  type in python

---
+++ DataType/DataType.py
@@ -0,0 +1,45 @@
+#  all type in python
+# Text Type:	str
+# Numeric Types:	int, float, complex
+# Sequence Types:	list, tuple, range
+# Mapping Type:	dict
+# Set Types:	set, frozenset
+# Boolean Type:	bool
+# Binary Types:	bytes, bytearray, memoryview
+# None Type:	NoneType
+
+
+# text type
+# name = "ismail" #this is string python call is str
+
+# Numeric Type
+# isInit = 10 # this is integer and also js call it integer
+# isFloat = 10.5 # this also call float
+# complex is not available in js
+# x = 1j # this is complex is add
+
+# Sequence Types:	list, tuple, range
+# friends = ["read", "baizid", "hafijul"]  # this is list( js call it array)
+
+# newFriends = ("read", "baizid", "hafijul")  # this is tuple (js not available)
+# x = range(9) # this is range but i don't know about it
+# print(x)
+
+# x = {"name": "John", "age": 36} this is dict type like json
+# x = {"apple", "banana", "cherry"}this is "set"
+
+# x = frozenset({"apple", "banana", "cherry"})	frozenset
+
+# x = True	bool js boolean
+
+# x = b"Hello"	bytes
+# x = bytearray(5)	bytearray
+# x = memoryview(bytes(5))	memoryview
+# x = None	NoneType
+
+#  if i wat to Specific Data Type (like ts)
+
+# name = str("ismail")
+# print(name)
+# reName = int(10)
+# print(reName)
