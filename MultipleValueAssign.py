7710c7e3d8ab8dcc985c408f876c7088b791ce38
ismallhossendev <ismailhossaindev@gmail.com>
2022-12-28 13:52:50 +0600

    multipleValue assign

---
+++ MultipleValueAssign.py
@@ -0,0 +1,25 @@
+
+# Many Values to Multiple Variables
+
+# a,b,c = "abc", 10, "20"
+# print(a,b,type(b))
+
+
+# One Value to Multiple Variables
+
+# myName = nickName = mainName = "John"
+# print(myName, nickName, mainName)
+
+# mainName = "John"
+# nickName = mainName
+# myName = nickName
+# print(myName, nickName, mainName)
+
+# Unpack a Collection (list) // JavaScript call this(array or object) destructuring
+
+
+# fruits = ["apple", "banana", "cherry"]
+# x, z,y = fruits # need to all upPack otherwise error
+# print(x)
+# print(z)
+# print(y)
